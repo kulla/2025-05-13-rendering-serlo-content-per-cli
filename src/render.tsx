71b8ecf9f610a9f5735805e788fa12c61a368dc4
Stephan Kulla <git.mail@kulla.me>
2025-05-13 11:00:16 +0200

    wip: Simulate DOM environment in NodeJs

--- src/render.tsx
+++ src/render.tsx
@@ -1,9 +1,28 @@
+import { JSDOM } from "jsdom";
 import React from "react";
-import { renderToString } from "react-dom/server";
-import { SerloRenderer } from "@serlo/editor";
+import { renderToStaticMarkup } from "react-dom/server";
+
+// Simulate a DOM environment
+const dom = new JSDOM("<!doctype html><html><body></body></html>");
+global.window = dom.window as any;
+// @ts-ignore
+global.window.requestAnimationFrame = () => {};
+global.document = dom.window.document;
+// @ts-ignore
+global.localStorage = { getItem: () => null };
+
+class EmptyResizeObserver {}
+// @ts-ignore
+global.ResizeObserver = EmptyResizeObserver;
+
+/*global.navigator = {
+  userAgent: "node.js",
+} as any;*/
+
+const { SerloRenderer } = require("@serlo/editor");
 
 const serloContentAsJson = {
-  plugin: "row",
+  plugin: "rows",
   state: [
     {
       plugin: "geogebra",
@@ -12,7 +31,7 @@ const serloContentAsJson = {
   ],
 };
 
-const html = renderToString(
+const html = renderToStaticMarkup(
   <SerloRenderer
     state={serloContentAsJson}
     language="de"
