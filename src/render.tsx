6272ee669ab16139c8cee3f5264f3060ef94accf
Stephan Kulla <git.mail@kulla.me>
2025-05-13 10:38:05 +0200

    feat: Add react && react-dom

--- src/render.tsx
+++ src/render.tsx
@@ -1,1 +1,6 @@
-console.log("Hello World");
+import React from "react";
+import { renderToString } from "react-dom/server";
+
+const html = renderToString(<h1>Hello world</h1>);
+
+console.log(html);
